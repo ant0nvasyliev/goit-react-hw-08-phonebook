4916f2c9427a99f9c7ddce2459a2575435204a78
ant0nvasyliev <90457867+ant0nvasyliev@users.noreply.github.com>
2023-12-06 13:11:05 +0000

    Deploying to gh-pages from @ ant0nvasyliev/goit-react-hw-08-phonebook@1ebee71ec28181cbf4566509c8b17e986b900fb1 🚀

---
+++ static/js/526.b2d2e7cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[526],{4526:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var r,o=t(168),a=t(5867).ZP.div(r||(r=(0,o.Z)(["\npadding-top: 50px;\n  text-align: center;\n"]))),c=t(184);function i(){return(0,c.jsx)(a,{children:(0,c.jsxs)("h1",{children:["Welcome to the phone book"," ",(0,c.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=526.b2d2e7cb.chunk.js.map
